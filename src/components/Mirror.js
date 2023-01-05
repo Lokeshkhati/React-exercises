d9ae878f61515fcb59e637415651758380eea256
Lokesh Khati <lokeshkhati005@gmail.com>
2023-01-05 14:22:23 +0530

    initial commit -m

---
+++ src/components/Mirror.js
@@ -0,0 +1,7 @@
+// Question 1: Create an input field and take string input in it from the user. Render the input value on the viewport. Create a button and on click of the button reverse the input and display it on the view.
+const Mirror = () => {
+    return (
+        <div>Mirror</div>
+    )
+}
+export default Mirror
