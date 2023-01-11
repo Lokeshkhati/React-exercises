545700935a401fd758585a1d2d69c69d4c89b402
Lokesh Khati <lokeshkhati005@gmail.com>
2023-01-11 20:41:16 +0530

    feat : create ProfileForm

---
+++ src/components/ProfileForm.js
@@ -0,0 +1,15 @@
+// Make a form, which will take the following 3 inputs from the user,  name, email, and mobile number
+// And the form will have a save button at the end.
+
+// When the user completes the form, the user should be able to save it.
+// After saving data, **show a success message.**
+
+// After data is submitted input fields should be disabled.
+
+// Then, Besides every field, there should be an edit button, which allows users to edit the field and save it.
+const ProfileForm = () => {
+  return (
+    <div>ProfileForm</div>
+  )
+}
+export default ProfileForm
