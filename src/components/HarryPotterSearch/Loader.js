e03c52b462e4281681d593eb225b15cb793ea5eb
Lokesh Khati <lokeshkhati005@gmail.com>
2023-02-14 20:38:12 +0530

    feat: add Loader

---
+++ src/components/HarryPotterSearch/Loader.js
@@ -0,0 +1,13 @@
+const Loader = () => {
+    return (
+      <div class="spinner-container">
+        <div class="spinner">
+          <div></div>
+          <div></div>
+        </div>
+      </div>
+    );
+  };
+  
+  export default Loader;
+  
