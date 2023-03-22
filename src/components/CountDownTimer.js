6556b28727730268b09a937c4dcae0f9abff3790
Lokesh Khati <lokeshkhati005@gmail.com>
2023-03-22 11:06:29 +0530

    refactor : refactor countdown

--- src/components/CountDownTimer.js
+++ src/components/CountDownTimer.js
@@ -2,38 +2,59 @@ import { useEffect, useState } from "react"
 
 const CountDownTimer = () => {
 
-    const [inputValue, setInputValue] = useState('')
-    const [time, setTime] = useState(inputValue * 60 * 24)
-    const [isCountingDown, setIsCountingDown] = useState(false)
+    const [inputValue, setInputValue] = useState(12)
+    const [time, setTime] = useState(inputValue * 60)
+    const [start, setStart] = useState(false)
+    const [pause, setPause] = useState(false)
 
-    // useEffect(() => {
-    //     setTimeout(() => {
-    //         setTime(time - 1000)
-    //     }, 1000)
+    const initialTime = inputValue * 60
+    useEffect(() => {
+        if (start) {
+            let timeId = setInterval(() => {
+                setTime(prevTime => prevTime - 1)
+            }, 1000)
 
-    // }, [time])
+            return () => clearInterval(timeId)
+        }
 
-    let timerId;
+    }, [start])
+
+    const formateTime = (totalSeconds) => {
+        let totalMinutes = Math.floor(totalSeconds / 60)
+        let seconds = totalSeconds % 60;
+        let minutes = totalMinutes % 60
+        let hours = Math.floor(totalMinutes / 60);
+
+        if (hours < 10) hours = `0${hours}`
+        if (minutes < 10) minutes = `0${minutes}`
+        if (seconds < 10) seconds = `0${seconds}`
+
+        return { h: hours, m: minutes, s: seconds, }
+    }
+
+    const { h, m, s } = formateTime(time)
+
+    useEffect(() => {
+        document.title = time ? `⌛ ${m} m : ${s} s` : 'Pomodoro app'
+    }, [time])
+
     const startTimerHandler = () => {
-        timerId = setIsCountingDown(true)
-        setTimeout(() => {
-            setTime(time - 1000)
-        }, 1000)
+        setStart(true)
     }
-    const stopTimerHandler = () => {
-        setIsCountingDown(false)
-        clearTimeout(timerId)
+    const resetTimerHandler = () => {
+        setTime(initialTime); setStart(false); setPause(false)
     }
-
     return (
         <div>
-            <input type='number' placeholder="Enter days" value={inputValue} onChange={(event) => setInputValue(event.target.value)} />
+            {/* <input type='number' placeholder="Enter duration" value={inputValue} onChange={(event) => setInputValue(event.target.value)} /> */}
 
-            {isCountingDown ? <button onClick={startTimerHandler}>stop</button> : <button onClick={stopTimerHandler}>start</button>}
-
-            <h1>{time} </h1>
-
-        </div>
+            {<h1>{` ${m} m : ${s} s`} </h1>}
+            {start ?
+                <button onClick={() => { setPause(true); setStart(false) }}>Pause</button> :
+                pause ? <button onClick={startTimerHandler}>Resume</button> : <button onClick={startTimerHandler}>start</button>
+            }
+            <button onClick={resetTimerHandler}>reset</button>
+        </div >
     )
 }
 export default CountDownTimer
